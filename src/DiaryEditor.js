48a7b647d7a4a920806751192239044658e6a191
hskimstudy <hskimstudy@gmail.com>
2023-02-24 10:44:32 +0900

    useMemo
    +getDiaryAnalysis

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -63,6 +63,7 @@ const DiaryEditor = ({ onCreate }) => {
         />
       </div>
       <div>
+        <span>오늘의 감정점수 : </span>
         <select
           name="emotion"
           value={state.emotion}
